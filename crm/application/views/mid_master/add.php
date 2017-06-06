53a8a53753a983032015290b8e2d4ca1ceea1d84
Rony Dasgupta <dasgupta.rony@gmail.com>
2017-06-06 16:26:02 +0530

    Mid Master

--- crm/application/views/mid_master/add.php
+++ crm/application/views/mid_master/add.php
@@ -8,41 +8,70 @@
 	</div><br/>  
 	<!---------------------Form Section Starts-------------------------->
 	<form id="frmMain" name="frmMain" method="post" action="<?php echo base_url().$this->controllerFile;?>insert" enctype="multipart/form-data">     
- 	
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Name</label>
-			<input type="text" class="form-control" id="module" name="module" placeholder="Module Name" value="" required>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Parent</label>
-			<select class="form-control" id="parent" name="parent" required>
-				<option value="0">Root</option>
-				<?php
-					$getModules=$this->db->query("select id, module from ".MODULE." where status='Y'");
-					foreach($getModules->result() as $val){ ?>				
-						<option value="<?php echo $val->id;?>"><?php echo $val->module;?></option>
-					<?php	
-					}
-				?>
+      		<div class="form-group">
+			<label for="exampleInputEmail1">Payment Type*</label>
+			<select class="form-control" id="paymentType" name="paymentType" required="required">
+				<option value="">Select</option>
+				<option value="credit_card">Credit Card</option>
+				<option value="echecking">EChecking</option>
 			</select>
-		</div>		
+		</div>    		
 		<div class="form-group">
-			<label for="exampleInputEmail1">Module Weightage</label>
-			<input type="text" class="form-control" id="weightage" name="weightage" placeholder="Module Weightage" value="" required>
-		</div>		
+			<label for="exampleInputEmail1">Gateway*</label>
+			<input type="text" class="form-control" id="gatewayID" name="gatewayID" placeholder="Gateway" required="required" value="">
+		</div>
 		<div class="form-group">
-			<label for="exampleInputEmail1">Module Link</label>
-			<input type="text" class="form-control" id="moduleLink" name="moduleLink" placeholder="Module Link" value="" required>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Description</label>
-			<input type="text" class="form-control" id="moduleDesc" name="moduleDesc" placeholder="Module Description" value="" required>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Image Class</label>
-			<input type="text" class="form-control" id="imageClass" name="imageClass" placeholder="Module Image Class" value="" required>
+			<label for="exampleInputEmail1">Gateway Type</label>
+			<input type="text" class="form-control" id="gatewayType" name="gatewayType" placeholder="Gateway Type" value="" >	
 		</div>	
-
+		<div class="form-group">
+			<label for="exampleInputEmail1">Program Name</label>
+			<input type="text" class="form-control" id="programName" name="programName" placeholder="Program Name" value="">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Directory</label>
+			<input type="text" class="form-control" id="directory" name="directory" placeholder="Directory" value="">
+		</div>	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Descriptor</label>
+			<input type="text" class="form-control" id="descriptor" name="descriptor" placeholder="Descriptor" value="">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Username*</label>
+			<input type="text" class="form-control" id="username" name="username" placeholder="Username" required="required" value="">
+		</div>	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Password*</label>
+			<input type="text" class="form-control" id="password" name="password" placeholder="Password" required="required" value="">
+		</div>	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Mid Number</label>
+			<input type="text" class="form-control" id="mid_number" name="mid_number" placeholder="Mid Number" value="">
+		</div>	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Key</label>
+			<input type="text" class="form-control" id="mid_key" name="mid_key" placeholder="Key" value="">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Link</label>
+			<input type="text" class="form-control" id="link" name="link" placeholder="Link" value="">
+		</div>	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Monthly Volume</label>
+			<input type="text" class="form-control" id="monthly_volume" name="monthly_volume" placeholder="Monthly Volume" value="">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Daily Volume</label>
+			<input type="text" class="form-control" id="daily_volume" name="daily_volume" placeholder="Daily Volume" value="">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Daily High Ticket Capture Limit*</label>
+			<input type="number" class="form-control" id="dailyHighTicketCapture" name="dailyHighTicketCapture" placeholder="Daily High Ticket Capture Limit" value="" required>
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Max Sales Amount*</label>
+			<input type="number" step="any" class="form-control" id="MaxSalesAmount" name="MaxSalesAmount" placeholder="Max Sales Amount" value="" required>
+		</div>		
 		<div class="form-group">
 			<label for="exampleInputEmail1">Status*</label>
 			<select class="form-control" id="status" name="status" required="required">
@@ -50,35 +79,24 @@
 				<option value="Y" >Active</option>
 				<option value="N" >In-Active</option>
 			</select>
-		</div>
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Actions</label>
-			<br/>
-			<?php 
-				$getActions=$this->db->query("select * from t_action where status='Y'");
-				foreach($getActions->result() as $valAction){ ?>
-					<input type="checkbox" name="action[]" value="<?php echo $valAction->id;?>"/><?php echo $valAction->action;?><br/>
-					
-					<?php
-					$getAdminLevels=$this->db->query("SELECT * FROM t_adminLevel where status='Y'");
-					foreach($getAdminLevels->result() as $adminLevels){
-						echo $adminLevels->level.'<br/>';
-						$getAdminType=$this->db->query("SELECT * FROM t_adminType where status='Y'");
-						foreach($getAdminType->result() as $adminType){ ?>
-							<input type="checkbox" name="permission[]" value="<?php echo $valAction->id;?>,<?php echo $adminLevels->id;?>,<?php echo $adminType->id;?>"/>
-							<?php
-							echo $adminType->type.'&nbsp;&nbsp;&nbsp;';
-						}
-						echo '<br/>';
-					}
-					echo '<br/>';
-					?>
-				<?php
-				}
-			?>
-			
 		</div>		
-		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Visibility*</label>
+			<select class="form-control" id="visibility" name="visibility" required="required">
+				<option value="">Select</option>
+				<option value="Y" >Y</option>
+				<option value="N" >N</option>
+			</select>
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Cron*</label>
+			<select class="form-control" id="cron" name="cron" required="required">
+				<option value="">Select</option>
+				<option value="Y" >Y</option>
+				<option value="N" >N</option>
+			</select>
+		</div>
+      
       
       <button type="submit" class="btn btn-default">Submit</button>
     </form>
--- crm/application/views/mid_master/edit.php
+++ crm/application/views/mid_master/edit.php
@@ -6,52 +6,96 @@
 	</div><br/>  
 	<!---------------------Form Section Starts-------------------------->
 	<form id="frmMain" name="frmMain" method="post" action="<?php echo base_url().$this->controllerFile;?>update" enctype="multipart/form-data">     
-		<input type="hidden" name="id" id="id" value="<?php echo $query['id']; ?>" /> 
+    <input type="hidden" name="id" id="id" value="<?php echo $query['id']; ?>" />
+          	<div class="form-group">
+			<label for="exampleInputEmail1">Payment Type*</label>
+			<select class="form-control" id="paymentType" name="paymentType" required="required">
+				<option value="">Select</option>
+				<option value="credit_card" <?php if($query['paymentType']=="credit_card"){?> selected <?php } ?> >Credit Card</option>
+				<option value="echecking" <?php if($query['paymentType']=="echecking"){?> selected <?php } ?>>EChecking</option>
+			</select>
+		</div>           
 		<div class="form-group">
-			<label for="exampleInputEmail1">Module Name*</label>
-			<input type="text" class="form-control" id="module" name="module" placeholder="First Name" required="required" value="<?php echo $query['module']; ?>">
+			<label for="exampleInputEmail1">Gateway*</label>
+			<input type="text" class="form-control" id="gatewayID" name="gatewayID" placeholder="gateway" required="required" value="<?php echo $query['gatewayID']; ?>">
 		</div>
 		<div class="form-group">
-			<label for="exampleInputEmail1">Module Parent</label>
-			<select class="form-control" id="parent" name="parent" required>
-				<option value="0">Root</option>
-				<?php
-					$getModules=$this->db->query("select id, module from ".MODULE." where status='Y'");
-					foreach($getModules->result() as $val){ ?>				
-						<option <?php if($query['parent']==$val->id){ echo 'selected'; } ?> value="<?php echo $val->id;?>"><?php echo $val->module;?></option>
-					<?php	
-					}
-				?>
-			</select>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Weightage</label>
-			<input type="text" class="form-control" id="weightage" name="weightage" placeholder="Module Weightage" value="<?php echo $query['weightage']; ?>" required>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Link</label>
-			<input type="text" class="form-control" id="moduleLink" name="moduleLink" placeholder="Module Link" value="<?php echo $query['moduleLink']; ?>" required>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Description</label>
-			<input type="text" class="form-control" id="moduleDesc" name="moduleDesc" placeholder="Module Description" value="<?php echo $query['moduleDesc']; ?>" required>
-		</div>		
-		<div class="form-group">
-			<label for="exampleInputEmail1">Module Image Class</label>
-			<input type="text" class="form-control" id="imageClass" name="imageClass" placeholder="Module Image Class" value="<?php echo $query['imageClass']; ?>" required>
+			<label for="exampleInputEmail1">Gateway Type</label>
+			<input type="text" class="form-control" id="gatewayType" name="gatewayType" placeholder="Gateway Type" value="<?php echo $query['gatewayType']; ?>" >	
 		</div>	
-
+		<div class="form-group">
+			<label for="exampleInputEmail1">Program Name</label>
+			<input type="text" class="form-control" id="programName" name="programName" placeholder="programName" value="<?php echo $query['programName']; ?>">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Directory</label>
+			<input type="text" class="form-control" id="directory" name="directory" placeholder="Directory" value="<?php echo $query['directory']; ?>">
+		</div>	
+		<div class="form-group">
+			<label for="exampleInputEmail1">Descriptor</label>
+			<input type="text" class="form-control" id="descriptor" name="descriptor" placeholder="Descriptor" value="<?php echo $query['descriptor']; ?>">
+		</div>			
+		<div class="form-group">
+			<label for="exampleInputEmail1">Username*</label>
+			<input type="text" class="form-control" id="username" name="username" placeholder="Username" value="<?php echo $query['username']; ?>">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Password*</label>
+			<input type="text" class="form-control" id="password" name="password" placeholder="Password" value="<?php echo $query['password']; ?>">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Mid Number</label>
+			<input type="text" class="form-control" id="mid_number" name="mid_number" placeholder="Mid Number" value="<?php echo $query['mid_number']; ?>">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Key</label>
+			<input type="text" class="form-control" id="mid_key" name="mid_key" placeholder="Key" value="<?php echo $query['mid_key']; ?>">
+		</div>			
+		<div class="form-group">
+			<label for="exampleInputEmail1">Link</label>
+			<input type="text" class="form-control" id="link" name="link" placeholder="Link" value="<?php echo $query['link']; ?>">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Monthly Volume</label>
+			<input type="text" class="form-control" id="monthly_volume" name="monthly_volume" placeholder="Monthly Volume" value="<?php echo $query['monthly_volume']; ?>">
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Daily Volume</label>
+			<input type="text" class="form-control" id="daily_volume" name="daily_volume" placeholder="Daily Volume" value="<?php echo $query['daily_volume']; ?>">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputEmail1">Daily High Ticket Capture Limit*</label>
+			<input type="number" class="form-control" id="dailyHighTicketCapture" name="dailyHighTicketCapture" placeholder="Daily High Ticket Capture Limit" value="<?php echo $query['dailyHighTicketCapture']; ?>" required>
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Max Sales Amount*</label>
+			<input type="number" step="any" class="form-control" id="MaxSalesAmount" name="MaxSalesAmount" placeholder="Max Sales Amount" value="<?php echo $query['MaxSalesAmount']; ?>" required>
+		</div>		
 		<div class="form-group">
 			<label for="exampleInputEmail1">Status*</label>
 			<select class="form-control" id="status" name="status" required="required">
 				<option value="">Select</option>
-				<option value="Y" <?php if($query['status']=='Y'){ echo 'selected'; } ?> >Active</option>
-				<option value="N" <?php if($query['status']=='N'){ echo 'selected'; } ?> >In-Active</option>
+				<option value="Y" <?php if($query['status']=="Y"){?> selected="selected"<?php } ?> >Active</option>
+				<option value="N" <?php if($query['status']=="N"){?> selected="selected"<?php } ?> >In-Active</option>
 			</select>
 		</div>
- 
-		<button type="submit" class="btn btn-default">Submit</button>  
-      
+		<div class="form-group">
+			<label for="exampleInputEmail1">Visibility*</label>
+			<select class="form-control" id="visibility" name="visibility" required="required">
+				<option value="">Select</option>
+				<option value="Y" <?php if($query['visibility']=="Y"){?> selected="selected"<?php } ?> >Y</option>
+				<option value="N" <?php if($query['visibility']=="N"){?> selected="selected"<?php } ?> >N</option>
+			</select>
+		</div>		
+		<div class="form-group">
+			<label for="exampleInputEmail1">Cron*</label>
+			<select class="form-control" id="cron" name="cron" required="required">
+				<option value="">Select</option>
+				<option value="Y" <?php if($query['cron']=="Y"){?> selected="selected"<?php } ?> >Y</option>
+				<option value="N" <?php if($query['cron']=="N"){?> selected="selected"<?php } ?> >N</option>
+			</select>
+		</div>		
+		<button type="submit" class="btn btn-default">Submit</button>
     </form> 
 <?php $this->load->view('footer');?>
 		
